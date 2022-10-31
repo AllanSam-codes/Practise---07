4c046ef480398f0b065c19f73d22a13b74bb922e
AllanSam <93815162+AllanSam-codes@users.noreply.github.com>
2022-10-31 16:18:05 +0530

    updated url

---
+++ index.js
@@ -0,0 +1,13 @@
+var output1 = document.querySelector("#output-text1")
+var output2 = document.querySelector("#output-text2")
+
+var url = "https://quick-access-api.desaihetav.repl.co"
+
+
+fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        output1.innerHTML = `<p>${data.message}</p>`
+        output2.innerHTML = `<p>${data.success}</p>`
+
+    })
